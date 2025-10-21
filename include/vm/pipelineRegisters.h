b68229ae033bd98a602542eeddcfaa2cb18abda1
Spark143-coder <vasantomkari1750@gmail.com>
2025-10-21 15:28:52 +0530

    Modification: Added few more attributes and member funcions definitions to header file

--- include/vm/pipelineRegisters.h
+++ include/vm/pipelineRegisters.h
@@ -2,31 +2,71 @@
 
 class pipelineRegister{
     private:
-        std::string instruction;
-        std::string readData1;
-        std::string readData2;
-        std::string immediate;
-        std::string executeSignal;
-        std::string memory;
-        std::string writeBack;
+        uint32_t instruction;
+        uint64_t readData1;
+        uint64_t readData2;
+        uint8_t rs2;
+        uint8_t rd;
+        int64_t immediate;
+        uint executeSignal;
+        uint memRead;
+        uint memWrite;
+        uint writeBack;
+        uint isBranch;
+        uint8_t opcode;
+        uint8_t funct3;
+        alu::AluOp aluOperation;
+        int64_t executionResult;
+        int64_t memoryResult;
+        uint isFloat;
+        uint isDouble;
+        uint isCSR;
     public:
-        pipelineRegister(std::string instruction,std::string readData1,std::string readData2,std::string immediate,std::string memory,std::string executeSignal,std::string writeBack);
+        pipelineRegister(uint32_t instruction,uint64_t readData1,uint64_t readData2,int64_t immediate,uint memRead,uint memWrite,uint executeSignal,uint writeBack,uint8_t opcode,uint8_t funct3);
         void Reset();
-        std::string readInstruction() const;
-        std::string ReadData1() const;
-        std::string ReadData2() const;
-        std::string readImmediate() const;
-        std::string ExecuteSignal() const;
-        std::string MemorySignal() const;
-        std::string WriteBackSignal() const;
+        uint32_t readInstruction() const;
+        uint64_t ReadData1() const;
+        uint64_t ReadData2() const;
+        int64_t readImmediate() const;
+        uint ExecuteSignal() const;
+        uint MemRead() const;
+        uint MemWrite() const;
+        uint WriteBackSignal() const;
+        alu::AluOp readAluOp() const;
+        uint8_t readOpcode() const;
+        uint8_t readFunct3() const;
+        int64_t readExecutionResult() const;
+        int64_t readMemoryResult() const;
+        uint readIsBranch() const;
+        uint readIsFloat() const;
+        uint readIsDouble() const;
+        uint readIsCSR() const;
+        uint8_t readRd() const;
+        uint8_t readRs2() const;
 
-        void fetchInstruction(std::string instruction);
-        void modifyReadData1(std::string readData1);
-        void modifyReadData2(std::string readData2);
-        void modifyImmediate(std::string immediate);
-        void modifyMemorySignal(std::string memory);
-        void modifyexecuteSignal(std::string executeSignal);
-        void modifyWriteBackSignal(std::string writeBack);
-        
+        void fetchInstruction(uint32_t instruction);
+        void modifyReadData1(uint64_t readData1);
+        void modifyReadData2(uint64_t readData2);
+        void modifyImmediate(int64_t immediate);
+        void modifyMemRead(uint MemRead);
+        void modifyMemWrite(uint MemWrite);
+        void modifyexecuteSignal(uint executeSignal);
+        void modifyWriteBackSignal(uint writeBack);
+        void modifyAluOp(alu::AluOp aluOp);
+        void modifyOpcode(uint8_t opcode);
+        void modifyFunct3(uint8_t funct3);
+        void modifyExecutionResult(int64_t executionResult);
+        void modifyMemoryResult(int64_t memoryResult);
+        void modifyIsBranch(uint isBranch);
+        void modifyIsFloat(uint isFloat);
+        void modifyIsDouble(uint isDouble);
+        void modifyIsCSR(uint isCSR);
+        void modifyRd(uint8_t Rd);
+        void modifyRs2(uint8_t rs2);
 };
+
+extern pipelineRegister IF_ID;
+extern pipelineRegister ID_EX;
+extern pipelineRegister EX_MEM;
+extern pipelineRegister MEM_WB;
 
